0fc73b068a4da27242e52de29d293cc917373db3
Kurniaji Gunawan <kurniaji.gunawan@gmail.com>
2024-04-28 12:08:07 +0700

    feat(trace): add trace library

--- go.mod
+++ go.mod
@@ -1,16 +1,26 @@
 module github.com/aidapedia/devkit
 
-go 1.18
+go 1.19
 
 require (
 	github.com/google/uuid v1.6.0
 	github.com/spf13/viper v1.16.0
+	go.opentelemetry.io/otel v1.11.2
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.11.2
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.11.2
+	go.opentelemetry.io/otel/sdk v1.11.2
+	go.opentelemetry.io/otel/trace v1.11.2
 	go.uber.org/zap v1.21.0
+	google.golang.org/grpc v1.63.2
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 )
 
 require (
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.19.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
@@ -20,11 +30,16 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.11.2 // indirect
+	go.opentelemetry.io/proto/otlp v1.2.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
-	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/net v0.23.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240227224415-6ceb2ff114de // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240401170217-c3f982113cda // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ trace/trace.go
@@ -0,0 +1,87 @@
+package tracer
+
+import (
+	"context"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/trace"
+	"google.golang.org/grpc/credentials"
+
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+
+	env "github.com/aidapedia/devkit/environment"
+)
+
+// Span is a struct to handle span
+type Span struct {
+	span trace.Span
+}
+
+func InitTracer(serviceName string, collectorURL string, isSecured bool) (*otlptrace.Exporter, error) {
+	secureOption := otlptracegrpc.WithInsecure()
+
+	isSecured = false
+	if isSecured { // TODO add tls and fix it later
+		secureOption = otlptracegrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, ""))
+	}
+
+	exporter, err := otlptrace.New(
+		context.Background(),
+		otlptracegrpc.NewClient(
+			secureOption,
+			otlptracegrpc.WithEndpoint(collectorURL),
+		),
+	)
+
+	if err != nil {
+		return nil, err
+	}
+	resources, err := resource.New(
+		context.Background(),
+		resource.WithAttributes(
+			attribute.String("service", serviceName),
+			attribute.String("library.language", "go"),
+			attribute.String("library.language", "go"),
+			attribute.String("environment", env.Development),
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	otel.SetTracerProvider(
+		sdktrace.NewTracerProvider(
+			sdktrace.WithSampler(sdktrace.AlwaysSample()),
+			sdktrace.WithBatcher(exporter),
+			sdktrace.WithResource(resources),
+		),
+	)
+	return exporter, nil
+}
+
+// StartSpanFromContext create and start an opentracing and newrelic span from context
+func StartSpanFromContext(ctx context.Context, operation string) (Span, context.Context) {
+	ctx, sp := otel.Tracer("").Start(ctx, operation)
+	return Span{
+		span: sp,
+	}, ctx
+}
+
+// Finish finish the span
+func (s *Span) Finish(errors *error) {
+	var message string
+	err := *errors
+	status := codes.Ok
+	if err != nil {
+		s.span.RecordError(err, trace.WithStackTrace(true))
+		message = err.Error()
+		status = codes.Error
+	}
+	s.span.SetStatus(status, message)
+	s.span.End()
+}
