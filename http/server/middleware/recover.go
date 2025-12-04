dc0a8c1492def1dadd823ccccbc0059be09571c1
Kurniaji Gunawan <kurniaji.gunawan@gmail.com>
2025-12-05 00:32:22 +0700

    feat: enhance recover middleware to log panic stack traces and return 500 JSON error responses.

--- http/server/middleware/recover.go
+++ http/server/middleware/recover.go
@@ -1,20 +1,34 @@
 package middleware
 
 import (
+	"fmt"
+	"net/http"
+	"runtime/debug"
+
+	gers "github.com/aidapedia/gdk/error"
+	ghttp "github.com/aidapedia/gdk/http"
 	"github.com/aidapedia/gdk/log"
 	"github.com/gofiber/fiber/v3"
-	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap"
 )
 
 // WithRecover is the middleware that recovers from panics.
 func WithRecover() fiber.Handler {
-	return func(c fiber.Ctx) error {
+	return func(c fiber.Ctx) (err error) {
 		defer func() {
 			if r := recover(); r != nil {
-				log.ErrorCtx(c.Context(), "Panic recovered", zapcore.Field{
-					Key:       "recover",
-					Interface: r,
-				})
+				var ok bool
+				var recErr error
+				if recErr, ok = r.(error); !ok {
+					recErr = fmt.Errorf("%v", r)
+				}
+				log.ErrorCtx(c.Context(), "Panic recovered", zap.Any("error", recErr), zap.ByteString("stack", debug.Stack()))
+				// We ignore the error returned by JSONResponse and return nil to the fiber handler
+				// because we have already written the response. Returning an error here would
+				// trigger Fiber's default error handler, which might overwrite our response.
+				_ = ghttp.JSONResponse(c, nil, gers.NewWithMetadata(recErr, ghttp.Metadata(http.StatusInternalServerError, "Internal Server Error")))
+				err = nil
+				return
 			}
 		}()
 		return c.Next()
