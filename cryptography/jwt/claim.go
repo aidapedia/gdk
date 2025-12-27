d5f83b859887797ec8a771cb1fb23e85e6c498d4
Kurniaji Gunawan <kurniaji.gunawan@gmail.com>
2025-12-27 12:23:49 +0700

    feat: Add JWT claim constants for common JWT claims.

---
+++ cryptography/jwt/claim.go
@@ -0,0 +1,10 @@
+package jwt
+
+const (
+	MapClaimJWTID     = "jti"
+	MapClaimSubject   = "sub"
+	MapClaimIssuer    = "iss"
+	MapClaimExpiry    = "exp"
+	MapClaimIssuedAt  = "iat"
+	MapClaimNotBefore = "nbf"
+)
